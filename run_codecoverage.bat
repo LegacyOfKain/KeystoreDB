8a55c019f4877dce4fd679b89cb89e519366813a
LegacyOfKain <36923616+LegacyOfKain@users.noreply.github.com>
2024-12-07 02:57:38 +0100

    Update run_codecoverage.bat

--- run_codecoverage.bat
+++ run_codecoverage.bat
@@ -8,4 +8,8 @@ dotnet test --collect:"XPlat Code Coverage"
 
 reportgenerator -reports:"../**/coverage.cobertura.xml" -targetdir:"CoverageReport" -reporttypes:"html"
 
-pause
+for /d /r . %%d in (TestResults) do @if exist "%%d" rmdir /s /q "%%d"
+
+echo Coverage report generated successfully at CoverageReport\index.html
+
+start CoverageReport\index.html
